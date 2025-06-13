6dd63be9afde836a57f3830a68e7f4088d2ec62c
HanninenF <hanninenfredrik@gmail.com>
2025-06-13 23:53:38 +0200

    refactor(save button) place save button over keyboardview

--- src/components/LyricsForm/LyricsForm.tsx
+++ src/components/LyricsForm/LyricsForm.tsx
@@ -7,6 +7,8 @@ import {
   Text,
   ScrollView,
   KeyboardAvoidingView,
+  Platform,
+  Keyboard,
 } from "react-native";
 import AppPressable from "../ui/AppPressable";
 import { useLyricsForm } from "../../hooks/useLyricsForm";
@@ -20,29 +22,47 @@ export default function LyricsForm() {
 
   return (
     <>
-      <KeyboardAwareScrollView contentContainerStyle={styles.scrollView}>
-        {showMetadata && <SongMetaData />}
-        <AppPressable onPress={() => setShowMetadata(!showMetadata)}>
-          <AppText>
-            {!showMetadata ? "Show song info" : "Hide song info"}
-          </AppText>
-        </AppPressable>
+      {" "}
+      <KeyboardAvoidingView
+        style={styles.flex}
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 0} // justera efter headerhöjd om du har
+      >
+        <KeyboardAwareScrollView contentContainerStyle={styles.scrollView}>
+          {showMetadata && <SongMetaData />}
+          <AppPressable onPress={() => setShowMetadata(!showMetadata)}>
+            <AppText>
+              {!showMetadata ? "Show song info" : "Hide song info"}
+            </AppText>
+          </AppPressable>
 
-        {/* skriv i låtens text */}
-        <LyricsInput
-          onChangeText={(text) => handleSetContent(text)}
-          value={formValues.content}
-          placeHolder="write here"
-          label={formValues.title}
-        />
-        <AppPressable onPress={handleSave}>
-          <Text>Save</Text>
-        </AppPressable>
-      </KeyboardAwareScrollView>
+          {/* skriv i låtens text */}
+          <LyricsInput
+            onChangeText={(text) => handleSetContent(text)}
+            value={formValues.content}
+            placeHolder="write here"
+            label={formValues.title}
+          />
+        </KeyboardAwareScrollView>
+        <View>
+          <AppPressable
+            onPress={() => {
+              handleSave();
+              Keyboard.dismiss();
+            }}
+          >
+            <AppText>Save</AppText>
+          </AppPressable>
+        </View>
+      </KeyboardAvoidingView>
     </>
   );
 }
 const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+
   scrollView: {
     alignItems: "center",
   },
